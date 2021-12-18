8893d3a591b8b53351ebd54fb41a4801287d3b51
serkanozell <81927386+serkanozell@users.noreply.github.com>
2021-12-18 18:30:37 +0300

    Add project files.

---
+++ Practises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
