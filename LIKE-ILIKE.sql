2ad6302ffef694ff11c5a9b2a4dab07af6b620d8
serkanozell <81927386+serkanozell@users.noreply.github.com>
2022-06-15 21:37:57 +0300

    Create LIKE-ILIKE.sql

---
+++ LIKE-ILIKE.sql
@@ -0,0 +1,25 @@
+--country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+
+SELECT *
+FROM COUNTRY
+WHERE COUNTRY like 'A%a';
+
+--country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+
+SELECT *
+FROM COUNTRY
+WHERE COUNTRY like '_____n';
+
+--film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+
+SELECT *
+FROM FILM
+WHERE TITLE ~~ * '%t%t%t%t%';
+
+--film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+
+SELECT *
+FROM FILM
+WHERE TITLE like 'C%'
+	AND LENGTH > 90
+	AND RENTAL_RATE = 2.99;
